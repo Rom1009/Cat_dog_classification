115416fb0ce773f6a548f2167e07600149836f87
Rom1009 <quynguyencurit2002@gmail.com>
2024-07-05 00:47:29 +0700

    completed

--- app.py
+++ app.py
@@ -52,44 +52,44 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Xử lý ảnh
     processed_image = process_image(uploaded_file.name)
-    if processed_image == None:
+    # if processed_image == None:
+    st.write(
+        "<h2 style='text-align: center; color: white;'>Please put image into folder code</h2>",
+        unsafe_allow_html=True,
+    )
+    # else:
+    # Đọc ảnh đã tải lên
+    image = Image.open(uploaded_file)
+
+    # Hiển thị ảnh gốc
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    transforms_test = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+        ]
+    )
+
+    test_data = Cat_Dog_Dataset(images=processed_image, transform=transforms_test)
+    test_loader = DataLoader(test_data, batch_size=1, shuffle=False, drop_last=True)
+    # Hiển thị ảnh đã xử lý
+    iter_test = iter(test_loader)
+    img_test = next(iter_test)
+
+    preds_test = model(img_test)
+    img_test_permuted = img_test.permute(0, 2, 3, 1)
+    rounded_preds = preds_test.round()
+
+    types = rounded_preds[0].item()
+
+    if types == 1:
         st.write(
-            "<h2 style='text-align: center; color: white;'>Please put image into folder code</h2>",
+            "<h2 style='text-align: center; color: white;'>This is a dog </h2>",
             unsafe_allow_html=True,
         )
-    else:
-        # Đọc ảnh đã tải lên
-        image = Image.open(uploaded_file)
-
-        # Hiển thị ảnh gốc
-        st.image(image, caption="Uploaded Image", use_column_width=True)
-
-        transforms_test = transforms.Compose(
-            [
-                transforms.ToTensor(),
-                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
-            ]
+    elif types == 0:
+        st.write(
+            "<h2 style='text-align: center; color: white;'>This is a cat</h2>",
+            unsafe_allow_html=True,
         )
-
-        test_data = Cat_Dog_Dataset(images=processed_image, transform=transforms_test)
-        test_loader = DataLoader(test_data, batch_size=1, shuffle=False, drop_last=True)
-        # Hiển thị ảnh đã xử lý
-        iter_test = iter(test_loader)
-        img_test = next(iter_test)
-
-        preds_test = model(img_test)
-        img_test_permuted = img_test.permute(0, 2, 3, 1)
-        rounded_preds = preds_test.round()
-
-        types = rounded_preds[0].item()
-
-        if types == 1:
-            st.write(
-                "<h2 style='text-align: center; color: white;'>This is a dog </h2>",
-                unsafe_allow_html=True,
-            )
-        elif types == 0:
-            st.write(
-                "<h2 style='text-align: center; color: white;'>This is a cat</h2>",
-                unsafe_allow_html=True,
-            )
